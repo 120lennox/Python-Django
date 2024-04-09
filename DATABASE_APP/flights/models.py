575def1cdf54a72b267b83e042e65a8663f215d5
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-09 15:10:46 +0200

    new table created, old table has foreign keys that reference to new table

--- DATABASE_APP/flights/models.py
+++ DATABASE_APP/flights/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+#Class airport code and the city to which the aiport belongs 
+class Airport(models.Model):
+    code = models.CharField(max_length=4)
+    city = models.CharField(max_length=64)
+
+#This class holds foreign keys of different Airports
 class Flights(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
     def __str__(self):
