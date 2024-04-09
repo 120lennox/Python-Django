cf7c9b418102521b1cea1dc9ae72aacfc9a27931
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-09 14:17:48 +0200

    database defined

--- DATABASE_APP/flights/models.py
+++ DATABASE_APP/flights/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Flights(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.origin} to {self.destination} in {self.duration}"
