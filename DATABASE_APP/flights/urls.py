3afefdb30f47629eb77b57227b28db6cb30b0466
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-10 19:36:23 +0200

    added a detail view to display details of each flight

--- DATABASE_APP/flights/urls.py
+++ DATABASE_APP/flights/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, FlightView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="index")
+    path("", HomePageView.as_view(), name="index"), 
+    path("<int:pk>", FlightView.as_view(), name="flight")
 ]
--- DATABASE_APP/flights/views.py
+++ DATABASE_APP/flights/views.py
@@ -1,7 +1,7 @@
 from typing import Any
 from django.shortcuts import render
 from .models import Airport, Flights
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -12,4 +12,13 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["flights"] = Flights.objects.all()
         return context #fixed bug. Template View if given a get context behavior returns the context
+
+class FlightView(DetailView):
+    template_name = 'flights/flight.html'
+
+    def get_context_data(flight_id):
+        context = super().get_context_data()
+        context["flight"] = Flights.objects.get(pk=flight_id)
+        return context
+        
     
